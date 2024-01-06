91923090890a68ab92968d532a42943101c943dd
Abbassi Zied <12896224+abbassizied@users.noreply.github.com>
2024-01-06 11:22:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.jsx')
 module.exports = withNextra({ 
     output: 'export',
     images: {
